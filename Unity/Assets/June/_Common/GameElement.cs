a015f6452c612834fb41613d59ee64e0aec5e109
Sandy <developer.pooch@gmail.com>
2016-10-09 13:43:39 +0530

    More Stripping...

--- Unity/Assets/June/_Common/GameElement.cs
+++ Unity/Assets/June/_Common/GameElement.cs
@@ -6,56 +6,58 @@ using June.Core;
 
 
 
-public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
+public class GameElementManager : BaseConfig<GameElementManager,GameElement>
+{
 
 	public const string FILE_NAME = "jsons/gameelement";
-    public const string ROOT_FIELD_NAME = "gameelement";
+	public const string ROOT_FIELD_NAME = "gameelement";
 
 
-		/// <summary>
-		/// Gets the name of the resource.
-		/// </summary>
-		/// <value>The name of the resource.</value>
-		public override string ResourceName {
-			get {
-						return FILE_NAME;
-			}
+	/// <summary>
+	/// Gets the name of the resource.
+	/// </summary>
+	/// <value>The name of the resource.</value>
+	public override string ResourceName {
+		get {
+			return FILE_NAME;
 		}
-		
-		/// <summary>
-		/// Gets the root key.
-		/// </summary>
-		/// <value>The root key.</value>
-		public override string RootKey {
-			get {
-				return ROOT_FIELD_NAME;
-			}
+	}
+
+	/// <summary>
+	/// Gets the root key.
+	/// </summary>
+	/// <value>The root key.</value>
+	public override string RootKey {
+		get {
+			return ROOT_FIELD_NAME;
 		}
-		
-		/// <summary>
-		/// Gets the item converter.
-		/// </summary>
-		/// <returns>The item converter.</returns>
-		/// <typeparam name="U">The 1st type parameter.</typeparam>
-		/// <value>The item converter.</value>
-		public override System.Converter<System.Collections.Generic.IDictionary<string, object>, GameElement> ItemConverter {
-			get {
-						return (doc) => new GameElement(doc);
-			}
+	}
+
+	/// <summary>
+	/// Gets the item converter.
+	/// </summary>
+	/// <returns>The item converter.</returns>
+	/// <typeparam name="U">The 1st type parameter.</typeparam>
+	/// <value>The item converter.</value>
+	public override System.Converter<System.Collections.Generic.IDictionary<string, object>, GameElement> ItemConverter {
+		get {
+			return (doc) => new GameElement (doc);
 		}
+	}
 
 
 	public BaseCollection<GameElement> _Elements;
+
 	/// <summary>
 	/// Gets the elements.
 	/// </summary>
 	/// <value>The elements.</value>
 	public BaseCollection<GameElement> Elements {
 		get {
-			if(null == _Elements) {
-				_Elements = new BaseCollection<GameElement>(
-					(SimpleJson.JsonArray)_Record[ROOT_FIELD_NAME], 
-					doc => new GameElement(doc));
+			if (null == _Elements) {
+				_Elements = new BaseCollection<GameElement> (
+					(SimpleJson.JsonArray)_Record [ROOT_FIELD_NAME], 
+					doc => new GameElement (doc));
 			}
 			return _Elements;
 		}
@@ -71,25 +73,26 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 		}
 	}
 
-#if UNITY_EDITOR
+	#if UNITY_EDITOR
 	private static  string[] _GameElementStrings;
+
 	/// <summary>
 	/// Sets the game element strings.
 	/// </summary>
 	/// <value>The game element strings.</value>
 	public static string[] GameElementStrings {
 		get {
-			if(null == _GameElementStrings && null != GameElements) {
+			if (null == _GameElementStrings && null != GameElements) {
 				_GameElementStrings = new string[GameElements.Count];
-				int index=0;
-				foreach(var ge in GameElements) {
-					_GameElementStrings[index++] = ge.ToString();
+				int index = 0;
+				foreach (var ge in GameElements) {
+					_GameElementStrings [index++] = ge.ToString ();
 				}
 			}
 			return _GameElementStrings;
 		}
 	}
-#endif
+	#endif
 
 
 	/// <summary>
@@ -97,15 +100,17 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 	/// </summary>
 	/// <returns>The game element by identifier.</returns>
 	/// <param name="id">Identifier.</param>
-	public static GameElement GetGameElementById(string id) {
+	public static GameElement GetGameElementById (string id)
+	{
 
-		return Util.FirstOrDefault(GameElements, g => g.Id == id);
+		return Util.FirstOrDefault (GameElements, g => g.Id == id);
 	}
 
-	public static List<GameElement> GetGameElementsByIds(IEnumerable<string> ids) {
-		List<GameElement> elements = new List<GameElement>();
-		foreach(var id in ids) {
-			elements.Add(GetGameElementById(id));
+	public static List<GameElement> GetGameElementsByIds (IEnumerable<string> ids)
+	{
+		List<GameElement> elements = new List<GameElement> ();
+		foreach (var id in ids) {
+			elements.Add (GetGameElementById (id));
 		}
 		return elements;
 	}
@@ -115,8 +120,9 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 	/// </summary>
 	/// <returns>The game elements by identifiers.</returns>
 	/// <param name="ids">Identifiers.</param>
-	public static List<GameElement> GetGameElementsByIds(List<string> ids) {
-		return GetGameElementsByIds(ids);
+	public static List<GameElement> GetGameElementsByIds (List<string> ids)
+	{
+		return GetGameElementsByIds (ids);
 	}
 
 	/// <summary>
@@ -124,18 +130,20 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 	/// </summary>
 	/// <returns>The game elements by currency type.</returns>
 	/// <param name="currencyType">Currency type.</param>
-	public static List<GameElement> GetGameElementsByCurrencyType(string currencyType) {
-		return Util.FilterList(GameElements, ge => ge.Currency.CanBePurchasedUsingCurrency(currencyType));
+	public static List<GameElement> GetGameElementsByCurrencyType (string currencyType)
+	{
+		return Util.FilterList (GameElements, ge => ge.Currency.CanBePurchasedUsingCurrency (currencyType));
 	}
 
-		/// <summary>
-		/// Gets the type of the game elements by currency.
-		/// </summary>
-		/// <returns>The game elements by currency type.</returns>
-		/// <param name="currencyType">Currency type.</param>
-		public static List<GameElement> GetGameElementWithCollectibleCurrency(string collectibleId) {
-				return Util.FilterList(GameElements, ge => ge.Currency.CanBePurchasedUsingCurrency("collectible") && ge.Currency.GetCurrencyByCurrencyType("collectible").CollectibleTypeId == collectibleId);
-		}
+	/// <summary>
+	/// Gets the type of the game elements by currency.
+	/// </summary>
+	/// <returns>The game elements by currency type.</returns>
+	/// <param name="currencyType">Currency type.</param>
+	public static List<GameElement> GetGameElementWithCollectibleCurrency (string collectibleId)
+	{
+		return Util.FilterList (GameElements, ge => ge.Currency.CanBePurchasedUsingCurrency ("collectible") && ge.Currency.GetCurrencyByCurrencyType ("collectible").CollectibleTypeId == collectibleId);
+	}
 
 	/// <summary>
 	/// Accepts the gift.
@@ -143,11 +151,12 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 	/// <returns><c>true</c>, if gift was accepted, <c>false</c> otherwise.</returns>
 	/// <param name="id">Identifier.</param>
 	/// <param name="value">Value.</param>
-	public static bool AcceptGift(string id, int value) {
+	public static bool AcceptGift (string id, int value)
+	{
 		bool status = false;
-		var ge = GetGameElementById(id);
-		if(null != ge) {
-			status = AcceptGift(ge, value);
+		var ge = GetGameElementById (id);
+		if (null != ge) {
+			status = AcceptGift (ge, value);
 		}
 		return status;
 	}
@@ -157,19 +166,21 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 	/// </summary>
 	/// <returns>The element type count.</returns>
 	/// <param name="type">Type.</param>
-	public static int GameElementTypeCount(string type) {
+	public static int GameElementTypeCount (string type)
+	{
 		return null != GameElements
-			? Util.Count<GameElement>(GameElements, ge => ge.Type == type)
+			? Util.Count<GameElement> (GameElements, ge => ge.Type == type)
 			: 0;
 	}
-	
+
 
 	/// <summary>
 	/// Accepts the gift.
 	/// </summary>
 	/// <returns><c>true</c>, if gift was accepted, <c>false</c> otherwise.</returns>
 	/// <param name="ge">Ge.</param>
-	public static bool AcceptGift(GameElement ge, int value) {
+	public static bool AcceptGift (GameElement ge, int value)
+	{
 		bool status = false;
 		// TODO Update this once API is added
 
@@ -222,8 +233,9 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 	/// </summary>
 	/// <returns><c>true</c>, if to unlocked content was added, <c>false</c> otherwise.</returns>
 	/// <param name="id">Identifier.</param>
-	public static bool AddToUnlockedContent(string id) {
-		return AddToUnlockedContent(GetGameElementById(id));
+	public static bool AddToUnlockedContent (string id)
+	{
+		return AddToUnlockedContent (GetGameElementById (id));
 	}
 
 	/// <summary>
@@ -231,7 +243,8 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 	/// </summary>
 	/// <returns><c>true</c>, if to unlocked content was added, <c>false</c> otherwise.</returns>
 	/// <param name="ge">Ge.</param>
-	public static bool AddToUnlockedContent(GameElement ge) {
+	public static bool AddToUnlockedContent (GameElement ge)
+	{
 		bool status = false;
 //		if(null != ge) {
 //			switch(ge.Type) {
@@ -259,14 +272,16 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 	/// <returns>The count of type.</returns>
 	/// <param name="gameElementIds">Game element identifiers.</param>
 	/// <param name="type">Type.</param>
-	public static int GetCountOfType(List<string> gameElementIds, string type) {
-		return Util.Count(GetGameElementsByIds(gameElementIds), ge => ge.Type == type);
+	public static int GetCountOfType (List<string> gameElementIds, string type)
+	{
+		return Util.Count (GetGameElementsByIds (gameElementIds), ge => ge.Type == type);
 	}
 
 	#if UNITY_EDITOR
-	public static int IndexOf(string gameElementId) {
-		for(int index=0; index<GameElements.Count; index++) {
-			if(0 == string.Compare(gameElementId, GameElements[index].Id, true)) {
+	public static int IndexOf (string gameElementId)
+	{
+		for (int index = 0; index < GameElements.Count; index++) {
+			if (0 == string.Compare (gameElementId, GameElements [index].Id, true)) {
 				return index;
 			}
 		}
@@ -278,37 +293,38 @@ public class GameElementManager : BaseConfig<GameElementManager,GameElement> {
 /// <summary>
 /// Game element.
 /// </summary>
-public partial class GameElement : BaseModel {
+public partial class GameElement : BaseModel
+{
 
 	public string Id {
 		get {
-			return GetString("_id");
+			return GetString ("_id");
 		}
 	}
 
 	public string Type {
 		get {
-			return GetString("tp");
+			return GetString ("tp");
 		}
 	}
 
 	public string Description {
 		get {
-			return GetString("ds");
+			return GetString ("ds");
 		}
 	}
 
 	public int Value {
 		get {
-			return GetInt("va");
+			return GetInt ("va");
 		}
 	}
-		
 
-	public bool IsConsumable{
-		get{
+
+	public bool IsConsumable {
+		get {
 			
-			return GetBool("ic");
+			return GetBool ("ic");
 		}
 	}
 
@@ -316,25 +332,26 @@ public partial class GameElement : BaseModel {
 	
 	public GameElement Parent {
 		get {
-			if(GameElementType.Color == this.Type) {
+			if (GameElementType.Color == this.Type) {
 				// GameElement colours are of the following format `IGL-005-001`
 				// We need to tazke `005` and combine it with `IGR` to find the corresponding character.
-				string parentId = string.Format("IGR-{0}", this.Id.Substring(4, 3));
-				return GameElementManager.GetGameElementById(parentId);
+				string parentId = string.Format ("IGR-{0}", this.Id.Substring (4, 3));
+				return GameElementManager.GetGameElementById (parentId);
 			}
 			return null;
 		}
 	}
 
 	private GameElementCurrencyCollection _Currency;
+
 	/// <summary>
 	/// Gets the currency.
 	/// </summary>
 	/// <value>The currency.</value>
 	public GameElementCurrencyCollection Currency {
 		get {
-			if(null == _Currency) {
-				_Currency = new GameElementCurrencyCollection((SimpleJson.JsonArray)_Record["co"], doc => new GameElementCurrency(doc));
+			if (null == _Currency) {
+				_Currency = new GameElementCurrencyCollection ((SimpleJson.JsonArray)_Record ["co"], doc => new GameElementCurrency (doc));
 			}
 			return _Currency;
 		}
@@ -346,8 +363,8 @@ public partial class GameElement : BaseModel {
 	/// <value><c>true</c> if this instance is custom currency; otherwise, <c>false</c>.</value>
 	public bool IsCustomCurrency {
 		get {
-			foreach(var c in Currency) {
-				if(c.Type == GameElementCurrencyType.CUSTOM)
+			foreach (var c in Currency) {
+				if (c.Type == GameElementCurrencyType.CUSTOM)
 					return true;
 			}
 			return false;
@@ -362,23 +379,23 @@ public partial class GameElement : BaseModel {
 	/// <value>The before purchase CTA count.</value>
 	public int BeforePurchaseCTACount {
 		get {
-			return LocalStore.Instance.GetIntOrDefault(string.Format(LocalStorageKeys.CTA_BEFORE_PURCHASE_COUNT_FORMAT, this.Id));
+			return LocalStore.Instance.GetIntOrDefault (string.Format (LocalStorageKeys.CTA_BEFORE_PURCHASE_COUNT_FORMAT, this.Id));
 		}
 		set {
-			LocalStore.Instance.SetInt(string.Format(LocalStorageKeys.CTA_BEFORE_PURCHASE_COUNT_FORMAT, this.Id), value);
+			LocalStore.Instance.SetInt (string.Format (LocalStorageKeys.CTA_BEFORE_PURCHASE_COUNT_FORMAT, this.Id), value);
 		}
 	}
-	
+
 	/// <summary>
 	/// Gets or sets a value indicating this instance's can purchase CTA count.
 	/// </summary>
 	/// <value><c>true</c> if this instance can purchase CTA count; otherwise, <c>false</c>.</value>
 	public int CanPurchaseCTACount {
 		get {
-			return LocalStore.Instance.GetIntOrDefault(string.Format(LocalStorageKeys.CTA_CAN_PURCHASE_COUNT_FORMAT, this.Id));
+			return LocalStore.Instance.GetIntOrDefault (string.Format (LocalStorageKeys.CTA_CAN_PURCHASE_COUNT_FORMAT, this.Id));
 		}
 		set {
-			LocalStore.Instance.SetInt(string.Format(LocalStorageKeys.CTA_CAN_PURCHASE_COUNT_FORMAT, this.Id), value);
+			LocalStore.Instance.SetInt (string.Format (LocalStorageKeys.CTA_CAN_PURCHASE_COUNT_FORMAT, this.Id), value);
 		}
 	}
 
@@ -386,15 +403,17 @@ public partial class GameElement : BaseModel {
 	/// Initializes a new instance of the <see cref="GameElement"/> class.
 	/// </summary>
 	/// <param name="doc">Document.</param>
-	public GameElement(IDictionary<string, object> doc) : base(doc) { }
+	public GameElement (IDictionary<string, object> doc) : base (doc)
+	{
+	}
 
-	public static GameElement GetElementBy(string id, string type, int value, string currency, float currencyValue) {
+	public static GameElement GetElementBy (string id, string type, int value, string currency, float currencyValue)
+	{
 		var dict = new Dictionary<string, object> () {
 			{ "_id", id },
 			{ "tp", type },
 			{ "ds", string.Empty },
-			{ "va", value },
-			{ "co", new SimpleJson.JsonArray() {
+			{ "va", value }, { "co", new SimpleJson.JsonArray () {
 					new Dictionary<string, object> () {
 						{ "c", currency },
 						{ "v", currencyValue }
@@ -410,7 +429,8 @@ public partial class GameElement : BaseModel {
 	/// Returns a <see cref="System.String"/> that represents the current <see cref="GameElement"/>.
 	/// </summary>
 	/// <returns>A <see cref="System.String"/> that represents the current <see cref="GameElement"/>.</returns>
-	public override string ToString () {
+	public override string ToString ()
+	{
 		return string.Format ("[GameElement: Id={0}, Type={1}, Description={2}, Value={3}]", Id, Type, Description, Value);
 	}
 
@@ -419,23 +439,27 @@ public partial class GameElement : BaseModel {
 /// <summary>
 /// Game element currency collection.
 /// </summary>
-public class GameElementCurrencyCollection : BaseCollection<GameElementCurrency> {
+public class GameElementCurrencyCollection : BaseCollection<GameElementCurrency>
+{
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="GameElementCurrencyCollection"/> class.
 	/// </summary>
 	/// <param name="array">Array.</param>
 	/// <param name="ctor">Ctor.</param>
-	public GameElementCurrencyCollection(SimpleJson.JsonArray array, Func<IDictionary<string, object>, GameElementCurrency> ctor) : base(array, ctor) { }
+	public GameElementCurrencyCollection (SimpleJson.JsonArray array, Func<IDictionary<string, object>, GameElementCurrency> ctor) : base (array, ctor)
+	{
+	}
 
 	/// <summary>
 	/// Gets the type of the currency by currency.
 	/// </summary>
 	/// <returns>The currency by currency type.</returns>
 	/// <param name="currencyType">Currency type.</param>
-	public GameElementCurrency GetCurrencyByCurrencyType(string currencyType) {
+	public GameElementCurrency GetCurrencyByCurrencyType (string currencyType)
+	{
 		foreach (GameElementCurrency item in _Items) {
-			if(item.Type == currencyType) {
+			if (item.Type == currencyType) {
 				return item;
 			}
 		}
@@ -448,8 +472,9 @@ public class GameElementCurrencyCollection : BaseCollection<GameElementCurrency>
 	/// </summary>
 	/// <returns><c>true</c> if this instance can be purchased using currency the specified currencyType; otherwise, <c>false</c>.</returns>
 	/// <param name="currencyType">Currency type.</param>
-	public bool CanBePurchasedUsingCurrency(string currencyType) {
-		return null != GetCurrencyByCurrencyType(currencyType);
+	public bool CanBePurchasedUsingCurrency (string currencyType)
+	{
+		return null != GetCurrencyByCurrencyType (currencyType);
 	}
 
 	/// <summary>
@@ -458,9 +483,9 @@ public class GameElementCurrencyCollection : BaseCollection<GameElementCurrency>
 	/// <value><c>true</c> if this instance is free; otherwise, <c>false</c>.</value>
 	public bool IsFree {
 		get {
-			if(null != _Items) {
-				foreach(var currency in _Items) {
-					if(currency.Value > 0f) {
+			if (null != _Items) {
+				foreach (var currency in _Items) {
+					if (currency.Value > 0f) {
 						return false;
 					}
 				}
@@ -473,46 +498,38 @@ public class GameElementCurrencyCollection : BaseCollection<GameElementCurrency>
 /// <summary>
 /// Game element currency.
 /// </summary>
-public class GameElementCurrency : BaseModel {
+public class GameElementCurrency : BaseModel
+{
 	public string Type {
 		get {
-			return GetString("c");
+			return GetString ("c");
 		}
 	}
 
 	public float Value {
 		get {
-			return GetFloat("v");
+			return GetFloat ("v");
 		}
 	}
 
 	public string CollectibleTypeId {
-				get {
-						return GetString("i");
-				}
+		get {
+			return GetString ("i");
 		}
+	}
 
-	public GameElementCurrency(IDictionary<string, object> doc) : base(doc) { }
+	public GameElementCurrency (IDictionary<string, object> doc) : base (doc)
+	{
+	}
 }
 
 /// <summary>
 /// Game element type.
 /// </summary>
-public class GameElementType {
+public class GameElementType
+{
 
-	#if UNITY_EDITOR
-	public static readonly string[] TYPES = {
-		Ladoo,
-		Coin,
-		Outfit,
-		Color,
-		Taunt,
-		GameFeature,
-		CoinMultiplier,
-		DailyUseItem,
-		Offer
-	};
-	#endif
+	
 
 	public const string Ladoo = "ladoos";
 	public const string Coin = "coins";
@@ -536,7 +553,8 @@ public class GameElementType {
 
 }
 
-public class GameElementCurrencyType {
+public class GameElementCurrencyType
+{
 	public const string USD = "usd";
 	public const string Ladoo = "ladoos";
 	public const string Collectible = "collectible";
